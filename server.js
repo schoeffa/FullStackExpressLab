aac45859de7fcb65088697898e710a31a21f987f
aaron <schoeffa@gmail.com>
2018-11-26 10:34:33 -0500

    Initial commit to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const cart = require("./routes/cart.js");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("./public"));
 app.use(express.json());
